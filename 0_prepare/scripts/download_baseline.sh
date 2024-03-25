e846b652db645854ecc6880fc5429750d9d00d39
Roger Tu <rogertu@scripps.edu>
2024-03-24 20:57:21 -0700

    add more conditionals to speed up

--- 0_prepare/scripts/download_baseline.sh
+++ 0_prepare/scripts/download_baseline.sh
@@ -9,15 +9,24 @@ echo "Downloading baseline files"
 wget -r -nv -N -c ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/
 
 # Fix the directory sturcture
-echo "Fixing directory structure"
-pushd ftp.ncbi.nlm.nih.gov/pubmed/baseline/
-cp * ../../../
-popd
-#rm -rf ftp.ncbi.nlm.nih.gov
+if ! [[ $(ll *.xml.gz | wc -l) -gt 1218 ]];then
+    echo "Fixing directory structure"
+    pushd ftp.ncbi.nlm.nih.gov/pubmed/baseline/
+    cp * ../../../
+    popd
+    # rm -rf ftp.ncbi.nlm.nih.gov
+else
+    echo "Directory structure is OK"
+fi
 
 # Make sure all the files are OK
 echo "Checking md5 checksum"
-cat *.md5 > all.md5
-md5sum -c all.md5
+ if [ -f $(find . -name all.md5) ]; then
+    echo "MD5 checksum file exists"
+else
+    echo "MD5 checksum file does not exist. Catting all md5 files to all.md5"
+    cat *.md5 > all.md5
+    md5sum -c all.md5
+fi
 
 echo "Completed downloading baseline"

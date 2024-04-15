aafb0a465b33a760adafb68f1d969986d2badeed
Roger Tu <rogertu@scripps.edu>
2024-04-15 15:01:56 -0700

    refactor init

--- tools/analysis_helper.py
+++ tools/analysis_helper.py
@@ -14,67 +14,159 @@ from typing import (
     cast,
 )
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import polars as pl
 import pykeen
+import pykeen.constants
 import pykeen.datasets.timeresolvedkg as trkg
 import pykeen.models
+import seaborn as sns
 import torch
 from pykeen.predict import predict_all, predict_target
 
-###
-# Helper functions for analysing pykeen outputs
-# for more information please see notebook: "03_generate_pykeen_output.ipynb"
+### Helper functions for analysing pykeen outputs
 
 ### Available functions
-# load_pykeen_train() -> initializes pykeen train set from a checkpoint
-# load_pykeen_test() -> initializes pykeen test set from a checkpoint
-# predict_on_test() -> generates tail predictions from loaded test h,r
-# get_rank_from_position() -> returns rank of known answers as a list for each triple
-# get_rank() -> returns rank of a series
-# get_mrr() -> returns a float, mean reciprocal rank of the entire test set based on known answers
-# get_hits_at_k() -> returns a float, reports the fraction of known answers less than or equal to k
 #
-###
+# load_pykeen_triples() ->      Imports PyKEEN triples factory based on build parameter and year subset.
+# predict_on() ->               Extracts predictions for 'test' or 'valid' sets given a pykeen model
+# get_answers() ->              Calculates a n-hot encoded list for the answers of a given query (head/tail)
+# get_rank() ->                 Returns the filtered rank
+# get_mrr() ->                  Returns the MRR score
+# get_hits_at_k() ->            Returns the number of hits at k
+# extract_answers_from_rank() -> Extracts answers from rank dataframe
+# extract_relative_year() ->    Extracts relative year from indications
+#
+# static methods
+# load_pykeen_dataset() ->      Imports PyKEEN dataset TRKG based on build parameter and year subset.
+# get_rank_from_position() ->   Gets positions of all hits and returns as an array. Accounts for ties, rank starts at 1.
+# get_true_index() ->           Gets the index of all hits and returns as as an array.
 
 
 class AnalysisHelper(object):
-    """ """
+    """
+    Get the predictions, true answer rank, hits_at_k, and MRR performance of a given model
+
+    Parameters
+    -----------
+    :build_dataset_kwargs: (dict)   parameters supplied to build the knolwedge graph
+    :pykeen_model:                  pykeen model used to make predictions
+    :chkpt_file: (str)              pykeen model checkpoint file name
+    :group: (str)                   evaluate on 'test' or 'valid'?
+    :qa: (str)                      short for query_answer; predict on 'head', 'tail', or 'both'?
+    :cuda: (bool)                   use gpu or cpu?
+    """
 
     def __init__(
         self,
         build_dataset_kwargs: Optional[Mapping[str, Any]] = {},
         year: str = "1950",
-        train_models_swap: bool = False,
+        train_models_swap: Optional[bool] = False,
+        pykeen_model: Optional[pykeen.models.base.Model] = None,
+        chkpt_file: Optional[str] = "TransE_neg_1950.pt",
+        group: Optional[str] = "test",  # {'test','valid'},
+        qa: Optional[str] = "both",  # {'both','head','tail'}
+        cuda: Optional[bool] = False,
     ):
         self.build_dataset_kwargs = build_dataset_kwargs
         self.year = year
         self.train_models_swap = train_models_swap
-        self.dataset = self.load_pykeen_dataset()
+        self.dataset = self.load_pykeen_dataset(
+            build_dataset_kwargs=self.build_dataset_kwargs, year=self.year
+        )
+        self.group = group
+        self.query_answer = qa
+        self.pykeen_model = pykeen_model
+        # self.model_kwargs = model_kwargs
+        self.chkpt_dir = pykeen.constants.PYKEEN_CHECKPOINTS.joinpath(chkpt_file)
 
+        # check if query_answer is valid
+        if self.query_answer != None:
+            assert self.query_answer in [
+                "head",
+                "tail",
+                "both",
+            ], "query_answer must be one of ['head','tail','both']"
+
+        # check if group is valid
+        if self.group != None:
+            assert self.query_answer in [
+                "test",
+                "valid",
+                "both",
+            ], "query_answer must be one of ['test','valid','both']"
+
+        # load train/test/valid if ttv is true otherwise just train/test
         if self.build_dataset_kwargs.get("split_ttv", 0) != 0:
             self.train, self.test, self.valid = self.load_pykeen_triples()
         else:
             self.train, self.test = self.load_pykeen_triples()
 
-    def load_pykeen_dataset(self) -> "TimeResolvedKG":
+        # load pykeen model if model and checkpoint available
+        if self.pykeen_model != None:
+            # check if checkpoint exists
+            assert (
+                self.chkpt_dir.is_file()
+            ), f"Model checkpoint not found at `{self.chkpt_dir}`"
+
+            # create df from model predictions
+            if cuda:
+                # load checkpoint in torch
+                self.chkpt = torch.load(
+                    self.chkpt_dir, map_location=torch.device("cuda:0")
+                )
+                # load checkpoint in to model
+                self.pykeen_model.load_state_dict(self.chkpt["model_state_dict"])
+                # make predictions
+                self.df = self.predict_on(pykeen_model=self.pykeen_model.cuda(0))
+            else:
+                self.chkpt = torch.load(
+                    self.chkpt_dir, map_location=torch.device("cpu")
+                )
+                self.pykeen_model.load_state_dict(self.chkpt["model_state_dict"])
+                self.df = self.predict_on(pykeen_model=self.pykeen_model)
+
+            self.df = (
+                self.get_answers()
+            )  # returns a list of [1,0] where 1 denotes answer index
+            self.df = (
+                self.get_rank()
+            )  # returns a list of filtered ranks for each answer
+
+            self.answer_df = (
+                self.extract_answers_from_rank()
+            )  # returns expanded list of only answers
+            self.answer_df = (
+                self.extract_relative_year()
+            )  # returns year for rexpanded list of only answers
+
+            self.mrr = self.get_mrr()
+            self.hits_1 = self.get_hits_at_k(k=1)
+            self.hits_3 = self.get_hits_at_k(k=3)
+            self.hits_10 = self.get_hits_at_k(k=10)
+            self.hits_100 = self.get_hits_at_k(k=100)
+
+    @staticmethod
+    def load_pykeen_dataset(
+        build_dataset_kwargs: Mapping[str, any], year: str
+    ) -> "TimeResolvedKG":
         """
         Takes parameters used to build the dataset and a given year to import PykEEN dataset class
         """
 
-        return trkg.TimeResolvedKG(
-            build_dataset_kwargs=self.build_dataset_kwargs, year=self.year
-        )
+        return trkg.TimeResolvedKG(build_dataset_kwargs=build_dataset_kwargs, year=year)
 
     def load_pykeen_triples(
         self,
     ) -> Tuple["TriplesFactory", "TriplesFactory", Optional["TriplesFactory"]]:
         """
         Takes parameters used to build the dataset and a given year to import PykEEN triplesfactory
-
         """
-        the_dataset = self.load_pykeen_dataset()
+        the_dataset = self.load_pykeen_dataset(
+            build_dataset_kwargs=self.build_dataset_kwargs, year=self.year
+        )
 
         train = the_dataset.training
         test = the_dataset.testing
@@ -101,7 +193,6 @@ class AnalysisHelper(object):
     def predict_on(
         self,
         pykeen_model: pykeen.models.base.Model,
-        group: Optional[str] = "test",  # {test,valid},
         query_answer: Optional[str] = "both",  # {both,tail,head}
     ) -> pl.DataFrame:
         """
@@ -109,17 +200,17 @@ class AnalysisHelper(object):
         group can be 'test' or 'valid' splits depending on the group to be assessed
         query_answer can be 'both', 'tail', 'head'. 'both' returns both head and tail predictions, 'tail' returns only tail predictions, 'head' returns only head predictions
         """
-        self.group = group
-        self.query_answer = query_answer
+        query_answer = self.query_answer
+        group = self.group
 
         # setup train, test, valid if there is a validation set available
         if self.build_dataset_kwargs.get("split_ttv", 0) != 0:
-            train, test, valid = self.load_pykeen_triples()
+            train, _, _ = self.load_pykeen_triples()
         else:
             assert (
                 group == "test"
             ), "No validation set found, in `self.build_dataset_kwargs` please use test group"
-            train, test = self.load_pykeen_triples()
+            train, _ = self.load_pykeen_triples()
 
         # import test or valid triples depending on 'group'
         predict_path = (
@@ -131,14 +222,6 @@ class AnalysisHelper(object):
         # DataFrame for predictions
         predict_df = pd.read_csv(predict_path, sep="\t", names=["h", "r", "t"])
 
-        # check if query_answer is valid
-        if query_answer != None:
-            assert query_answer in [
-                "head",
-                "tail",
-                "both",
-            ], "query_answer must be one of ['head','tail','both']"
-            self.query_answer = query_answer
         # build a dictionary for each condition
         query_answer_dict = {
             "head": dict(zip(predict_df["t"], predict_df["r"])),
@@ -189,7 +272,6 @@ class AnalysisHelper(object):
                 t_res_ls.append(pred)
             df_t = pl.concat(t_res_ls)
 
-        torch.cuda.empty_cache()
         if query_answer == "head":
             df_h = (
                 df_h.with_columns(query=pl.lit("head"))
@@ -203,7 +285,6 @@ class AnalysisHelper(object):
                 )
             )
 
-            self.df = df_h
             return df_h
 
         elif query_answer == "tail":
@@ -219,7 +300,6 @@ class AnalysisHelper(object):
                 )
             )
 
-            self.df = df_t
             return df_t
 
         else:
@@ -242,7 +322,7 @@ class AnalysisHelper(object):
                     ]
                 )
             )
-            self.df = df
+
             return df
 
     def get_answers(self) -> pl.DataFrame:
@@ -349,20 +429,20 @@ class AnalysisHelper(object):
             df_h = df_h.group_by(["query_label", "query"], maintain_order=True).agg(
                 ["answer_label", "in_testing", "in_validation"]
             )
-            self.df = df_h
+
             return df_h
         elif self.query_answer == "tail":
             df_t = df_t.group_by(["query_label", "query"], maintain_order=True).agg(
                 ["answer_label", "in_testing", "in_validation"]
             )
-            self.df = df_t
+
             return df_t
         else:
             df = pl.concat([df_h, df_t])
             df = df.group_by(["query_label", "query"], maintain_order=True).agg(
                 ["answer_label", "in_testing", "in_validation"]
             )
-            self.df = df
+
             return df
 
     @staticmethod
@@ -394,7 +474,7 @@ class AnalysisHelper(object):
 
     def get_rank(
         self,
-    ) -> pd.DataFrame:
+    ) -> pl.DataFrame:
         """
         Takes a dataframe of PyKEEN model predictions and returns dataframe with list of  rank vals
         """
@@ -404,7 +484,7 @@ class AnalysisHelper(object):
         ), "No dataframe found, please run self.predict_on() first"
 
         res_col = "in_testing" if self.group == "test" else "in_validation"
-
+        drop_col = "in_validation" if self.group == "test" else "in_testing"
         if res_col not in self.df.columns:
             results_df = self.get_answers()
             df = results_df.unique(["query_label", "query"])
@@ -414,9 +494,9 @@ class AnalysisHelper(object):
         df = df.with_columns(
             pl.col(res_col)
             .map_elements(lambda x: self.get_rank_from_position(x))
-            .alias("known_ranks")
-        )
-        self.df = df
+            .alias("answer_filt_rank")
+        ).drop(drop_col)
+
         return df
 
     def get_mrr(
@@ -431,11 +511,11 @@ class AnalysisHelper(object):
 
         results_df = self.df
 
-        if "known_ranks" not in results_df.columns:
+        if "answer_filt_rank" not in results_df.columns:
             results_df = self.get_rank()
 
         all_ranks = [
-            float(j) for i in results_df["known_ranks"].to_list() for j in i
+            float(j) for i in results_df["answer_filt_rank"].to_list() for j in i
         ]  # np.reciprocal requires floats, using ints leads to unexpected behavior
         all_rr = np.reciprocal(all_ranks)
         mrr = np.mean(all_rr)
@@ -455,10 +535,12 @@ class AnalysisHelper(object):
 
         results_df = self.df
 
-        if "known_ranks" not in results_df.columns:
+        if "answer_filt_rank" not in results_df.columns:
             results_df = self.get_rank()
 
-        all_ranks = [float(j) for i in results_df["known_ranks"].to_list() for j in i]
+        all_ranks = [
+            float(j) for i in results_df["answer_filt_rank"].to_list() for j in i
+        ]
         k_array = np.full(np.array(all_ranks).shape, k, dtype=float)
         hitsk = np.mean(
             np.less_equal(
@@ -489,13 +571,13 @@ class AnalysisHelper(object):
 
         # get dataframe of query and answer labels
         results_df = (
-            results_df.explode(["known_ranks", "known_trues"])
+            results_df.explode(["answer_filt_rank", "known_trues"])
             .with_columns(
                 pl.col("answer_label").list.get(pl.col("known_trues")).alias("answers")
             )
             .drop(group)
         )
-        self.answers_df = results_df
+
         return results_df
 
     def extract_relative_year(self) -> pl.DataFrame:
@@ -503,11 +585,7 @@ class AnalysisHelper(object):
         Extracts relative year from indications
         """
         # get results from self.extract_answers_from_rank
-        assert (
-            type(self.answers_df) == pl.DataFrame
-        ), "No dataframe found, please run self.extract_answers_from_rank() first"
-
-        results_df = self.answers_df
+        results_df = self.answer_df
 
         # get local directory to import indications
         ind_dir = pathlib.Path(self.dataset.training_path).parent.joinpath(
@@ -532,13 +610,10 @@ class AnalysisHelper(object):
             )
 
         if self.query_answer == "head":
-            self.answers_df = results_df_head
             return results_df_head
 
         elif self.query_answer == "tail":
-            self.answers_df = results_df_tail
             return results_df_tail
         else:
             a_df = pl.concat([results_df_head, results_df_tail])
-            self.answers_df = a_df
             return a_df
